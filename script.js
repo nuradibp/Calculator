2688cdd1ed00c25bc74e9bcfd8bc3ec52bedb25d
nuradibphasya <nuradibphasya@gmail.com>
2022-06-13 22:00:05 +0700

    Edit pada script.js untuk menyimpan angka/operator untuk melakukan kalkulasi

--- script.js
+++ script.js
@@ -10,6 +10,33 @@ const numbers = document.querySelectorAll(".number");
 numbers.forEach((number) => {
   //   console.log(number);
   number.addEventListener("click", (event) => {
-    updateScreen(event.target.value);
+    inputNumber(event.target.value);
+    updateScreen(currentNumber);
   });
 });
+
+let prevNumber = "";
+let calculationOperator = "";
+let currentNumber = "0";
+
+const inputNumber = (number) => {
+  if (currentNumber === "0") {
+    currentNumber = number;
+  } else {
+    currentNumber += number;
+  }
+};
+
+const operators = document.querySelectorAll(".operator");
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", (event) => {
+    inputOperator(event.target.value);
+  });
+});
+
+const inputOperator = (operator) => {
+  prevNumber = currentNumber;
+  calculationOperator = operator;
+  currentNumber = "";
+};
